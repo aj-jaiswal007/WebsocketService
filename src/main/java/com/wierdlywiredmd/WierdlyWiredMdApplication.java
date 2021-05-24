7aaab71f3eed80cda4b339210dec583fc8bc4cc6
Amar Jaiswal <aj.jaiswal007@gmail.com>
2021-05-24 17:10:50 +0530

    Added starter project files

---
+++ src/main/java/com/wierdlywiredmd/WierdlyWiredMdApplication.java
@@ -0,0 +1,13 @@
+package com.wierdlywiredmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WierdlyWiredMdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WierdlyWiredMdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wierdlywiredmd/WierdlyWiredMdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wierdlywiredmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WierdlyWiredMdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
